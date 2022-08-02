da7240329332c4a4b444ee823a50881ece28b7a5
OlexxandrS <94759574+OlexxandrS@users.noreply.github.com>
2022-08-02 19:12:45 +0300

    level300

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //You can edit ALL of the code here
 const rootElem = document.getElementById("root");
-const search = document.querySelector("#search");
+const search = document.getElementById("search");
 const numOfEpisodes = document.querySelector(".display-number-of-episodes");
+const select = document.getElementById("select");
+let allEpisodesList = [];
 
 let searchValue = "";
 
@@ -17,6 +19,7 @@ function formatNumber(number) {
 }
 
 function makePageForEpisodes(episodeList) {
+  allEpisodesList = episodeList;
   for (let e of episodeList) {
     const episode = document.createElement("div");
     episode.className = "episode";
@@ -38,10 +41,16 @@ function makePageForEpisodes(episodeList) {
     episode.appendChild(a);
     episode.appendChild(summary);
     rootElem.appendChild(episode);
+
+    const option = document.createElement("option");
+    option.text =
+      `S${formatNumber(e.season)}E${formatNumber(e.number)}` + ` - ${e.name}`;
+    option.setAttribute("value", `${e.season} ${e.number}`);
+    select.appendChild(option);
   }
 }
 
-function OnSearch() {
+function OnSearch(event) {
   const allEpisodes = getAllEpisodes();
   searchValue = event.target.value.toLowerCase();
   let searchedEpisodes = allEpisodes.filter(
@@ -55,5 +64,38 @@ function OnSearch() {
 
   numOfEpisodes.innerHTML = `Displaying ${searchedEpisodes.length}/ ${allEpisodes.length}episodes`;
 }
+
+select.addEventListener("change", (event) => {
+  rootElem.replaceChildren([]);
+  let currentEpisode = `${event.target.value}`;
+
+  for (let e of allEpisodesList) {
+    if (
+      `${e.season} ${e.number}` == currentEpisode ||
+      currentEpisode == "all"
+    ) {
+      const episode = document.createElement("div");
+      episode.className = "episode";
+      const head = document.createElement("p");
+      head.className = "head";
+      const a = document.createElement("a");
+      const img = document.createElement("img");
+      const summary = document.createElement("div");
+      summary.className = "summary";
+
+      head.innerHTML =
+        `${e.name}` + ` S${formatNumber(e.season)}E${formatNumber(e.number)}`;
+      img.src = `${e.image.medium}`;
+      summary.innerHTML = `${e.summary}`;
+      a.href = `${e.url}`;
+
+      a.appendChild(img);
+      episode.appendChild(head);
+      episode.appendChild(a);
+      episode.appendChild(summary);
+      rootElem.appendChild(episode);
+    }
+  }
+});
 
 window.onload = setup;
